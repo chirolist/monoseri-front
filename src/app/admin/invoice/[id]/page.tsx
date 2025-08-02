78eabd53d668f30af8b78f897e998511553dc966
chirolist <document.top.value@gmail.com>
2025-08-03 01:54:48 +0900

    commit

--- src/app/admin/invoice/[id]/page.tsx
+++ src/app/admin/invoice/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { InvoiceTemplate } from 'components/template/invoice';
 
-export default async function Index({ params }: { params: { id: number } }) {
+export default async function Index({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
   const { id } = await params;
-  return <InvoiceTemplate id={id} />;
+  return <InvoiceTemplate id={Number(id)} />;
 }
