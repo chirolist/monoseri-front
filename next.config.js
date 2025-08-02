9614e1cf47120b7b40044d839becad79c8adb57d
chirolist <document.top.value@gmail.com>
2025-08-03 01:24:13 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+loadEnv(process.env.APP_ENV);
+
+const tsconfigPath =
+  process.env.NEXT_PUBLIC_APP_ENV === 'local'
+    ? './tsconfig.json'
+    : './tsconfig.build.json';
+
 /** @type {import('next').NextConfig} */
 
 // const withTM = require('next-transpile-modules')(['@babel/preset-react']);
@@ -10,6 +17,11 @@
 const nextConfig = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  reactStrictMode: false,
+  trailingSlash: true,
+  typescript: {
+    tsconfigPath: tsconfigPath,
+  },
   images: {
     domains: [
       'images.unsplash.com',
@@ -22,3 +34,17 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+/**
+ * @param {string} appEnv
+ */
+function loadEnv(appEnv = 'local') {
+  const env = {
+    ...require(`./env/env.${appEnv}`),
+    NEXT_PUBLIC_APP_ENV: appEnv,
+  };
+
+  Object.entries(env).forEach(([key, value]) => {
+    process.env[key] = value;
+  });
+}
