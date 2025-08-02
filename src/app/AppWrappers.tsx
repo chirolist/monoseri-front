f4f54edd76c9a733a003f7c969ae6893f2e531f7
chirolist <document.top.value@gmail.com>
2025-08-03 02:03:37 +0900

    修正

--- src/app/AppWrappers.tsx
+++ src/app/AppWrappers.tsx
@@ -7,16 +7,6 @@ import 'styles/Contact.css';
 import 'styles/MiniCalendar.css';
 import 'styles/index.css';
 
-import dynamic from 'next/dynamic';
-
-const _NoSSR: React.FC<{ children: ReactNode }> = ({ children }) => (
-  <React.Fragment>{children}</React.Fragment>
-);
-
-const NoSSR = dynamic(() => Promise.resolve(_NoSSR), {
-  ssr: false,
-});
-
 export default function AppWrappers({ children }: { children: ReactNode }) {
-  return <NoSSR>{children}</NoSSR>;
+  return <React.Fragment>{children}</React.Fragment>;
 }
