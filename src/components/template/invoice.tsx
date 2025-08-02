39ad21cce32171cbb7ce35b4a430895a7a5b4944
chirolist <document.top.value@gmail.com>
2025-08-02 21:31:35 +0900

    commit

--- src/components/template/invoice.tsx
+++ src/components/template/invoice.tsx
@@ -13,7 +13,7 @@ import {
   Collapse,
   Switch,
 } from '@mui/material';
-import { RiLineHeight } from 'react-icons/ri';
+import { CircularProgress, MenuItem, Select } from '@mui/material';
 import { sign } from 'crypto';
 
 type InvoiceProps = {
@@ -22,9 +22,11 @@ type InvoiceProps = {
 
 export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
   const [alertVisible, setAlertVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const [baseInfoVisible, setBaseInfoVisible] = useState(true);
   const [invoiceInfoVisible, setInvoiceInfoVisible] = useState(true);
+  const [billingInfoVisible, setBillingInfoVisible] = useState(true);
 
   const handleAlertClose = () => {
     setAlertVisible(false);
@@ -36,6 +38,10 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
 
   const handleInvoiceInfoToggle = () => {
     setInvoiceInfoVisible(!invoiceInfoVisible);
+  };
+
+  const handleBillingInfoToggle = () => {
+    setBillingInfoVisible(!billingInfoVisible);
   };
 
   const {
@@ -55,11 +61,19 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
       affiliation: '',
       invoiceCode: '',
       projectName: '',
+      billSyozoku: '',
+      billName: '',
+      prefecture: '',
+      salesStatus: '',
+      category: '',
     },
   });
 
-  const onSubmit = (data) => {
+  const onSubmit = async (data) => {
+    setLoading(true);
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     // Handle form submission logic here
+    setLoading(false);
     setAlertVisible(true);
     console.log('Form data submitted:', data);
   };
@@ -68,7 +82,29 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
 
   return (
     <Container maxWidth="xl" className="mt-5">
-      <form onSubmit={handleSubmit(onSubmit)} className="mt-5 grid h-full">
+      {loading && (
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            backgroundColor: 'rgba(255, 255, 255, 0.8)',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            zIndex: 1000,
+          }}
+        >
+          <CircularProgress />
+        </div>
+      )}
+
+      <form
+        onSubmit={handleSubmit(async (data) => await onSubmit(data))}
+        className="mt-5 grid h-full"
+      >
         {alertVisible && (
           <div
             style={{
@@ -95,6 +131,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
           <Grid
             item
             xs={12}
+            md={6}
             style={{ display: 'flex', justifyContent: 'flex-end' }}
           >
             <Button
@@ -125,7 +162,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
             />
             <Collapse in={baseInfoVisible}>
               <Grid container spacing={2}>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -142,7 +179,24 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                     <TextField fullWidth value={id} disabled size="small" />
                   </Box>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
+                  <Box display="flex" alignItems="center" width="100%">
+                    <Typography
+                      variant="body1"
+                      style={{
+                        marginRight: '8px',
+                        width: labelWidth,
+                        whiteSpace: 'nowrap',
+                        fontSize: '15px',
+                        textAlign: 'right',
+                      }}
+                    >
+                      ユーザーcode
+                    </Typography>
+                    <TextField fullWidth value={id} disabled size="small" />
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -167,7 +221,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                     />
                   </Box>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -204,7 +258,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                     />
                   </Box>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -241,7 +295,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                     />
                   </Box>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -279,7 +333,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                     />
                   </Box>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -317,7 +371,149 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                     />
                   </Box>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
+                  <Box display="flex" alignItems="center" width="100%">
+                    <Typography
+                      variant="body1"
+                      style={{
+                        marginRight: '8px',
+                        width: labelWidth,
+                        whiteSpace: 'nowrap',
+                        fontSize: '15px',
+                        textAlign: 'right',
+                      }}
+                    >
+                      カテゴリー
+                    </Typography>
+                    <TextField
+                      size="small"
+                      fullWidth
+                      {...register('category')}
+                      error={!!errors.category}
+                      helperText={errors.category ? 'Category is required' : ''}
+                    />
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Box display="flex" alignItems="center" width="100%">
+                    <Typography
+                      variant="body1"
+                      style={{
+                        marginRight: '8px',
+                        width: labelWidth,
+                        whiteSpace: 'nowrap',
+                        fontSize: '15px',
+                        textAlign: 'right',
+                      }}
+                    >
+                      都道府県{' '}
+                      <span
+                        style={{
+                          backgroundColor: 'red',
+                          color: 'white',
+                          padding: '2px 4px',
+                          borderRadius: '4px',
+                          marginLeft: '4px',
+                          fontSize: '12px',
+                          lineHeight: '1.4',
+                          verticalAlign: 'middle',
+                        }}
+                      >
+                        必須
+                      </span>
+                    </Typography>
+                    <Select
+                      size="small"
+                      fullWidth
+                      {...register('prefecture')}
+                      error={!!errors.prefecture}
+                      defaultValue=""
+                    >
+                      {[
+                        '北海道',
+                        '青森県',
+                        '岩手県',
+                        '宮城県',
+                        '秋田県',
+                        '山形県',
+                        '福島県',
+                        '茨城県',
+                        '栃木県',
+                        '群馬県',
+                        '埼玉県',
+                        '千葉県',
+                        '東京都',
+                        '神奈川県',
+                        '新潟県',
+                        '富山県',
+                        '石川県',
+                        '福井県',
+                        '山梨県',
+                        '長野県',
+                        '岐阜県',
+                        '静岡県',
+                        '愛知県',
+                        '三重県',
+                        '滋賀県',
+                        '京都府',
+                        '大阪府',
+                        '兵庫県',
+                        '奈良県',
+                        '和歌山県',
+                        '鳥取県',
+                        '島根県',
+                        '岡山県',
+                        '広島県',
+                        '山口県',
+                        '徳島県',
+                        '香川県',
+                        '愛媛県',
+                        '高知県',
+                        '福岡県',
+                        '佐賀県',
+                        '長崎県',
+                        '熊本県',
+                        '大分県',
+                        '宮崎県',
+                        '鹿児島県',
+                        '沖縄県',
+                      ].map((prefecture) => (
+                        <MenuItem key={prefecture} value={prefecture}>
+                          {prefecture}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Box display="flex" alignItems="center" width="100%">
+                    <Typography
+                      variant="body1"
+                      style={{
+                        marginRight: '8px',
+                        width: labelWidth,
+                        whiteSpace: 'nowrap',
+                        fontSize: '15px',
+                        textAlign: 'right',
+                      }}
+                    >
+                      営業ステータス
+                    </Typography>
+                    <Select
+                      size="small"
+                      fullWidth
+                      {...register('salesStatus')}
+                      defaultValue=""
+                    >
+                      {['営業中', '休業中', '準備中'].map((status) => (
+                        <MenuItem key={status} value={status}>
+                          {status}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -340,7 +536,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                     />
                   </Box>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -428,7 +624,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
             />
             <Collapse in={invoiceInfoVisible}>
               <Grid container spacing={2}>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -467,7 +663,7 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                     />
                   </Box>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={12} md={6}>
                   <Box display="flex" alignItems="center" width="100%">
                     <Typography
                       variant="body1"
@@ -503,6 +699,84 @@ export const InvoiceTemplate: React.FC<InvoiceProps> = ({ id }) => {
                       helperText={
                         errors.projectName ? 'ProjectName is required' : ''
                       }
+                    />
+                  </Box>
+                </Grid>
+              </Grid>
+            </Collapse>
+          </Card>
+
+          <Card className={`bg-white p-4`}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={billingInfoVisible}
+                  onChange={handleBillingInfoToggle}
+                />
+              }
+              label={<span style={{ fontWeight: 'bold' }}>請求者情報</span>}
+            />
+            <Collapse in={billingInfoVisible}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <Box display="flex" alignItems="center" width="100%">
+                    <Typography
+                      variant="body1"
+                      style={{
+                        marginRight: '8px',
+                        width: labelWidth,
+                        whiteSpace: 'nowrap',
+                        fontSize: '15px',
+                        textAlign: 'right',
+                      }}
+                    >
+                      所属
+                    </Typography>
+                    <TextField
+                      size="small"
+                      fullWidth
+                      {...register('billSyozoku')}
+                      error={!!errors.billSyozoku}
+                      helperText={
+                        errors.billSyozoku ? 'billSyozoku is required' : ''
+                      }
+                    />
+                  </Box>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                  <Box display="flex" alignItems="center" width="100%">
+                    <Typography
+                      variant="body1"
+                      style={{
+                        marginRight: '8px',
+                        width: labelWidth,
+                        whiteSpace: 'nowrap',
+                        fontSize: '15px',
+                        textAlign: 'right',
+                      }}
+                    >
+                      請求者名{' '}
+                      <span
+                        style={{
+                          backgroundColor: 'red',
+                          color: 'white',
+                          padding: '2px 4px',
+                          borderRadius: '4px',
+                          marginLeft: '4px',
+                          fontSize: '12px',
+                          lineHeight: '1.4',
+                          verticalAlign: 'middle',
+                        }}
+                      >
+                        必須
+                      </span>
+                    </Typography>
+                    <TextField
+                      size="small"
+                      fullWidth
+                      {...register('billName')}
+                      error={!!errors.billName}
+                      helperText={errors.billName ? 'billName is required' : ''}
                     />
                   </Box>
                 </Grid>
